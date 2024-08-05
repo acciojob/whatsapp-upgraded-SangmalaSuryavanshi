5b447d8c3d34be434c43bc40c7d8dc5bd5351f31
sangmalasurywanshi935@gamil.com <sangmalasurywanshi935@gmail.com>
2024-08-05 13:56:02 +0530

    spring 2 module test

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -30,6 +30,12 @@ public class WhatsappRepository {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
         //your code here
+        if(userMobile.contains(mobile)){
+            throw new Exception("User already exists");
+        }
+        User user = new User(name,mobile);
+        userMobile.add(mobile);
+        return "SUCCESS";
     }
 
     public Group createGroup(List<User> users){
@@ -37,13 +43,29 @@ public class WhatsappRepository {
         // If there are only 2 users, the group is a personal chat and the group name should be kept as the name of the second user(other than admin)
         // If there are 2+ users, the name of group should be "Group customGroupCount". For example, the name of first group would be "Group 1", second would be "Group 2" and so on.
         // If group is successfully created, return group.
-        //your code here
+        //your code
+        if(users.size() == 2){
+            Group gp = new Group(users.get(1).getName(),2);
+            groupUserMap.put(gp,users);
+            groupMessageMap.put(gp,new ArrayList<>());
+            return gp;
+        }else {
+            customGroupCount++;
+            Group gp = new Group("Group " + customGroupCount,users.size());
+            groupUserMap.put(gp,users);
+            groupMessageMap.put(gp,new ArrayList<>());
+            adminMap.put(gp,users.get(0));
+            return gp;
+        }
     }
 
     public int createMessage(String content){
         // The 'i^th' created message has message id 'i'.
         // Return the message id.
         //your code here
+        messageId++;
+        Message m = new Message(messageId,content,new Date());
+        return m.getId();
     }
 
     public int sendMessage(Message message, User sender, Group group) throws Exception{
@@ -51,6 +73,19 @@ public class WhatsappRepository {
         //Throw "You are not allowed to send message" if the sender is not a member of the group
         //If the message is sent successfully, return the final number of messages in that group.
         //your code here
+        if(!groupUserMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+        if(!groupUserMap.get(group).contains(sender)){
+            throw new Exception("You are not allowed to send message");
+        }
+        List<Message> messageList = groupMessageMap.get(group);
+        messageList.add(message);
+        groupMessageMap.put(group,messageList);
+
+        senderMap.put(message,sender);
+
+        return groupMessageMap.get(group).size();
     }
 
     public String changeAdmin(User approver, User user, Group group) throws Exception{
@@ -60,6 +95,17 @@ public class WhatsappRepository {
         //Change the admin of the group to "user" and return "SUCCESS".
 
         //your code here
+        if(!groupUserMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+        if(adminMap.get(group) != approver){
+            throw new Exception("Approver does not have rights");
+        }
+        if(!groupUserMap.get(group).contains(user)){
+            throw new Exception("User is not a participant");
+        }
+        adminMap.replace(group,user);
+        return "SUCCESS";
     }
 
     public int removeUser(User user) throws Exception{
@@ -68,11 +114,46 @@ public class WhatsappRepository {
         //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
         //If user is removed successfully, return (the updated number of users in the group + the updated number of messages in group + the updated number of overall messages)
         //your code here
+        for(Group gp: groupUserMap.keySet()) {
+            List<User> userList = groupUserMap.get(gp);
+            if (userList.contains(user)) {
+                for (User admin : adminMap.values()) {
+                    if (admin == user) {
+                        throw new Exception("Cannot remove admin");
+                    }
+                }
+                groupUserMap.get(gp).remove(user);
+
+                for (Message m: senderMap.keySet()){
+                    User u = senderMap.get(m);
+                    if(u == user) {
+                        senderMap.remove(m);
+                        groupMessageMap.get(gp).remove(m);
+                        return groupUserMap.get(gp).size() + groupMessageMap.get(gp).size() + senderMap.size();
+                    }
+                }
+            }
+        }
+        throw new Exception("User not found");
     }
 
     public String findMessage(Date start, Date end, int K) throws Exception{
         // Find the Kth latest message between start and end (excluding start and end)
         // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
         //your code here
+        TreeMap<Integer,String> map = new TreeMap<>();
+        ArrayList <Integer> list = new ArrayList<>();
+        for (Message m: senderMap.keySet()){
+            if( m.getTimestamp().compareTo(start) > 0 && m.getTimestamp().compareTo(end) < 0){
+                map.put(m.getId(),m.getContent());
+                list.add(m.getId());
+            }
+        }
+        if (map.size() < K){
+            throw new Exception("K is greater than the number of messages");
+        }
+        Collections.sort(list);
+        int k = list.get(list.size()-K);
+        return map.get(k);
     }
 }
